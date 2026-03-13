b898cf6fb8d630c0700c0793b1e32a2491c98a8c
greenpixels (Sven Teigler) <Sven.Teigler@outlook.com>
2026-03-13 22:41:30 +0100

    refactor for_each_in_directory to include max_depth parameter for recursive directory traversal

--- addons/dir_helper/dir_helper.gd
+++ addons/dir_helper/dir_helper.gd
@@ -1,20 +1,23 @@
 class_name DirHelper
 
-static func for_each_in_directory(directory: String, callback: Callable) -> void:
+
+static func for_each_in_directory(
+	directory: String, callback: Callable, max_depth: int = 1
+) -> void:
 	var dir: DirAccess = DirAccess.open(directory)
-	
+
 	if dir:
 		dir.list_dir_begin()
 		var file_name: String = dir.get_next()
-		
+
 		while file_name != "":
 			var file_path: String = directory + "/" + file_name
 			var resource_path: String = file_path.replace(".remap", "")
-			if dir.current_is_dir():
-				for_each_in_directory(file_path, callback)
+			if dir.current_is_dir() and max_depth > 0:
+				for_each_in_directory(file_path, callback, max_depth - 1)
 			elif resource_path.ends_with(".tres") or resource_path.ends_with(".res"):
 				var res_path: String = resource_path
 				var resource: Resource = ResourceLoader.load(res_path)
 				callback.call(resource)
-			
+
 			file_name = dir.get_next()
